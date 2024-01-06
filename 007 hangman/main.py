f43ef8de158632674e1e55127de1f6c053361d66
TonyVSK <arthurdovao5@gmail.com>
2024-01-06 13:36:52 -0300

    created environment

---
+++ 007 hangman/main.py
@@ -0,0 +1,9 @@
+from ascii import opening, hangman, skull, trophy
+from words import words
+from time import sleep
+
+
+
+lifes = 7
+
+while life >0
---
+++ 007 hangman/words.py
@@ -0,0 +1,3 @@
+words = [
+"functional","forget","smart","clerk","relevance","retailer","hurl","accurate","ample","storm","notorious","realism","or","give","oven","donor","attractive","labour","warn","mill"
+]
