c71983e664fb52ba7c951cdc8f6cb6a160c5aabd
TonyVSK <arthurdovao5@gmail.com>
2024-01-07 13:02:14 -0300

    project 9 complete

--- 009 secret auction/main.py
+++ 009 secret auction/main.py
@@ -20,14 +20,22 @@ people = []
 person = {}
 answer = 'yes'
 
+
+# add people from auction
 while answer == 'yes':
     clear()
     print(hammer)
     
     person['name'] = input('What is your name?\n')
-    person['bid'] = int(input('What is your bid?\n'))
+    person['bid'] = int(input('What is your bid?\n$'))
     people.append(person.copy())
     answer = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
 
+#see who won
+bigger = people[0]
+for each_person in people:
+    if each_person['bid']>bigger['bid']:
+        bigger=each_person
 
-print(people)
+clear()
+print(f"The winner is {bigger['name']} with a bid of {bigger['bid']}")
