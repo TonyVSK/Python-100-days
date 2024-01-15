b135ed58460ec6e17ccb2eb23ccc10fc6e4c799d
TonyVSK <arthurdovao5@gmail.com>
2024-01-15 15:31:58 -0300

    project done

--- 017 The Quiz Project/main.py
+++ 017 The Quiz Project/main.py
@@ -1,7 +1,16 @@
+from random import shuffle
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
+print("""
+  __   _  _  __  ____     ___   __   _  _  ____ 
+ /  \ / )( \(  )/ ___)   / __) / _\ ( \/ )(  __)
+(  O )) \/ ( )( \___ \  ( (_ \/    \/ \/ \ ) _) 
+ \__\)\____/(__)(____/   \___/\_/\_/\_)(_/(____)
+""")
+
+shuffle(question_data)
 question_bank = []
 
 for i in range(0, len(question_data)):
--- 017 The Quiz Project/quiz_brain.py
+++ 017 The Quiz Project/quiz_brain.py
@@ -3,7 +3,25 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
 
+
+#    def still_has_questions(self, list_height):
+#        if list_height > 0:
+#            return True
+#        else:
+#            return False
+
     def next_question(self):
-        current_question = self.question_list[self.question_number]
-        self.question_number+=1
-        input(f'Q. {self.question_number}: {current_question.text} (True/False): ').lower()
+        on = 'true'
+        counter = 0
+        list_height = len(self.question_list)
+        while on == 'true':
+            current_question = self.question_list[self.question_number]
+            self.question_number+=1
+            user = input(f'Q. {self.question_number}: {current_question.text} (True/False): ').lower()
+            if (user == 'true' and current_question.answer == 'True') or (user == 'false' and current_question.answer == 'False'):
+                on = 'true'
+                counter +=1
+                list_height -= 1
+            else:
+                print(f'Gamer over... you did {counter} points')
+                on = 'false'
