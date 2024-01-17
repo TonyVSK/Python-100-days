0754fc5c017b62768a30f44cf74ed1b561f449a8
TonyVSK <arthurdovao5@gmail.com>
2024-01-17 17:45:45 -0300

    clear function added

--- 019 Etch-A-Sketch/main.py
+++ 019 Etch-A-Sketch/main.py
@@ -21,10 +21,17 @@ def move_back():
     tim.backward(10)
 
 
+def clear_function():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
 
 screen.listen()
 screen.onkey(key ="w", fun=move_forward)
 screen.onkey(key ="a", fun=move_left)
 screen.onkey(key ="d", fun=move_right)
 screen.onkey(key ="s", fun=move_back)
+screen.onkey(key ="c", fun=clear_function)
 screen.exitonclick()
