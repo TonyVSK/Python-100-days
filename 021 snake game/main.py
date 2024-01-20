115ee5f05f351905bb59eedd6874921099f48397
TonyVSK <arthurdovao5@gmail.com>
2024-01-20 12:44:01 -0300

    project 21 created

---
+++ 021 snake game/main.py
@@ -0,0 +1,8 @@
+# There is 7 steps:
+# 1. Create a snake body
+# 2. Move the snake
+# 3. Control the snake
+# 4. Detect collision with food
+# 5. Create a scoreboard
+# 6. Detect collision with wall - GameOver
+# 7. Detect collision with tail - GameOver
