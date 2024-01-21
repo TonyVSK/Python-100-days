01cc28fa01a96cca4f9caa86278ae43bddb1ea4e
TonyVSK <arthurdovao5@gmail.com>
2024-01-21 10:15:12 -0300

    step 2 done

--- 021 snake game/main.py
+++ 021 snake game/main.py
@@ -8,18 +8,19 @@
 # 7. Detect collision with tail - GameOver
 
 from turtle import Turtle, Screen
+from time import sleep
 
 # Screen definitions
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snack Game')
+screen.tracer(0)
 
 
 # 1. Create a snake body
-snake = []
-x = 0
-y= 0
+segments = []
+starting_positions = [(0, 0),(-20, 0),(-40, 0)]
 
 for i in range(0, 3):
     new_segment = Turtle()
@@ -27,13 +28,22 @@ for i in range(0, 3):
     new_segment.speed('fastest')
     new_segment.shape('square')
     new_segment.color('white')
-    new_segment.goto(x, y)
-    snake.append(new_segment)
-    x -=20
+    new_segment.goto(starting_positions[i])
+    segments.append(new_segment)
 
 
 # 2. Move the snake
     
-    
+game_is_on = True
+while game_is_on:
+    screen.update()
+    sleep(0.1)
+
+    for i in range(len(segments)-1, 0, -1):  # no nosso caso, de 2 a 0, diminuir 1, ai ele pega a posição 2, 1 e 0
+        new_x = segments[i-1].xcor()        # novo x do segmento 2 será a coordenada x do segmento 1; novo x do segmento1 será do seg 0; novo x  do 0 seria  a do segmento negativo,
+        new_y = segments[i-1].ycor()        # mas como n tem, ele vai para o move forward na linha 46, depois o while volta. Y segue a mesma lógica
+        segments[i].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
 
 screen.exitonclick()
