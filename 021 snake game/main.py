285038fb34f18e3070dd88f0231b3efba61036b3
TonyVSK <arthurdovao5@gmail.com>
2024-01-22 13:51:28 -0300

    step 5 done

--- 021 snake game/main.py
+++ 021 snake game/main.py
@@ -11,6 +11,7 @@ from turtle import Screen
 from food import Food
 from snake import Snake
 from time import sleep
+from scoreboard import Scoreboard
 
 # Screen definitions
 screen = Screen()
@@ -27,7 +28,8 @@ starting_positions = [(0, 0),(-20, 0),(-40, 0)]
 snake = Snake()
 # 4. Detect collision with food
 food = Food()
-
+# 5. Create a scoreboard
+scoreboard = Scoreboard(0)
 # 3. Control the snake
 screen.listen() # it will able to screen understand buttons being pressed with up, down, left and right
 screen.onkey(snake.up, 'Up')
@@ -51,5 +53,8 @@ while game_is_on:
     # 4. Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+    # 5. Create a scoreboard
+        scoreboard.newScore()
+    
 
 screen.exitonclick()
---
+++ 021 snake game/scoreboard.py
@@ -0,0 +1,23 @@
+# 5. Create a scoreboard
+
+from turtle import Turtle
+
+
+
+
+class Scoreboard(Turtle):
+    def __init__(self, user):
+        super().__init__()
+        self.color('white')
+        self.penup()
+        self.goto(0, 240)
+        self.write('Scoreboard: 0', align='center', font=('Arial', 20, 'normal'))
+        self.hideturtle()
+        self.user = user
+        self.newValue = 0
+
+
+    def newScore(self):
+        self.clear()
+        self.newValue += 1
+        self.write(f'Scoreboard: {self.newValue}', align='center', font=('Arial', 20, 'normal'))
