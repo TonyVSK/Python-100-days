a3ab0278c418c6f4988476a9378b1e064e887685
TonyVSK <arthurdovao5@gmail.com>
2024-01-26 17:13:13 -0300

    squirrel exercicie done

--- 025 pandas library/025 pandas learning/main.py
+++ 025 pandas library/025 pandas learning/main.py
@@ -17,8 +17,8 @@
 
 
 
-import pandas as pd
-data = pd.read_csv("weather_data.csv")
+# import pandas as pd
+# data = pd.read_csv("weather_data.csv")
 
 
 
@@ -28,11 +28,36 @@ data = pd.read_csv("weather_data.csv")
 
 
 # Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+
+# data=pd.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+
+import pandas as pd
+
+counter=0
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels = data[data["Primary Fur Color"] == "Gray"]
+new_grey_squirrels=len(grey_squirrels)
+print(new_grey_squirrels)
+
+red_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+new_red_squirrels=len(red_squirrels)
+print(new_red_squirrels)
+
+black_squirrels = data[data["Primary Fur Color"] == "Black"]
+new_black_squirrels=len(black_squirrels)
+print(new_black_squirrels)
+
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    "Squirrel Colors": ["Grey", "Red", "Black"],
+    "Numbers": [new_grey_squirrels, new_red_squirrels, new_black_squirrels]
 }
 
-data=pd.DataFrame(data_dict)
-print(data)
-data.to_csv("new_data.csv")
+data_dict=pd.DataFrame(data_dict)
+data_dict.to_csv("squirrelCounter.csv")
