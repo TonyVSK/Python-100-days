6e1078b1331fa2e917787a7e1cc9ced0211df0e9
TonyVSK <arthurdovao5@gmail.com>
2024-01-30 16:46:50 -0300

    pomodoro completed

--- Python 100 days projects/028 Pomodoro Project/main.py
+++ Python 100 days projects/028 Pomodoro Project/main.py
@@ -10,13 +10,23 @@ CHECK_MARK = "✓"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    #timer_text 00:
+    canvas.itemconfig(timer_number, text="00:00")
+    # title_label "Timer"
+    timer_text.config(text="Timer", fg=GREEN)
+    #reset check_marks
+    check_marks.config(text = "")
 
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_counter():
     global reps
-    global timer_text
+    global timer_number
     reps += 1
     
 
@@ -26,27 +36,36 @@ def start_counter():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
-        Timer_text.config(text="Break", fg=RED)
+        timer_text.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        Timer_text.config(text="Break", fg=PINK)
+        timer_text.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
-        Timer_text.config(text="Work", fg=GREEN)
+        timer_text.config(text="Work", fg=GREEN)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(counter):
+    global reps
+    global timer
+
     counter_min = math.floor(counter/60)
     counter_sec = counter%60
     if counter_sec < 10:
         counter_sec = f"0{counter_sec}"
     if counter_min < 10:
         counter_min = f"0{counter_min}"
-    canvas.itemconfig(timer_text, text=f'{counter_min}:{counter_sec}')
+    canvas.itemconfig(timer_number, text=f'{counter_min}:{counter_sec}')
+    # canvas.itemconfig(timer_number, text=f'{counter_min}:{counter_sec}')
     if counter > 0:
-        window.after(1000, count_down, counter-1)
+        timer = window.after(1000, count_down, counter-1)
     else:
         start_counter()
-    
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for i in range(0, work_sessions):
+            mark += f"{CHECK_MARK}"
+
+        check_marks.config(text = mark)
 # ---------------------------- UI SETUP ------------------------------- #
 reps = 0
 window = Tk()
@@ -60,12 +79,12 @@ canvas.grid(row=0, column=1)
 # Creating image and text POMODORO TOMATO AND SCREEN
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-timer_text = canvas.create_text(100, 130, text = "00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_number = canvas.create_text(100, 130, text = "00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
 # Timer Text
-Timer_text = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW)
-Timer_text.grid(row=0, column=1)
+timer_text = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW)
+timer_text.grid(row=0, column=1)
 
 # Start and Reset buttons
 def button_clicked():
@@ -76,10 +95,10 @@ button1 = Button(text="Start", highlightthickness=0, command = start_counter)
 button1.grid(column=0, row=2)
 
 # reset button
-button2 = Button(text="Reset", command = button_clicked, highlightthickness=0)
+button2 = Button(text="Reset", highlightthickness=0, command = reset_timer)
 button2.grid(column=2, row=2)
 
-check_marks = Label(text = CHECK_MARK, fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
