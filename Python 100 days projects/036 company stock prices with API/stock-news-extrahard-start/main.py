ef88e3567c020a1248ad9c325c51dddf842ea8c3
TonyVSK <arthurdovao5@gmail.com>
2024-02-08 12:31:18 -0300

    obtaining updated results

--- Python 100 days projects/036 company stock prices with API/stock-news-extrahard-start/main.py
+++ Python 100 days projects/036 company stock prices with API/stock-news-extrahard-start/main.py
@@ -1,8 +1,40 @@
+from apikey import key
+import json
+
+APIKEY = key
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+import requests
+import datetime as dt
+# Defining day of week
+now = dt.datetime.now()
+day = now.day
+month = now.month
+year = now.year
+# print(day, month, year)
+# # "2024-02-07 19:00:00"
+# print(now)
+
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=60min&apikey={APIKEY}'
+r = requests.get(url)
+data = r.json()
+my_dict = data["Time Series (60min)"]
+lista = []
+for item in my_dict:
+    lista.append(item)
+
+updated_stock = f"{lista[0]}"
+
+print(my_dict[updated_stock])
+print(day, month, year)
+# # "2024-02-07 19:00:00"
+print(now)
+# print(data["Time Series (60min)"][0])
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
