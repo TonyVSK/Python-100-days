56a832cb9804927659132659d02104db97499fd9
TonyVSK <arthurdovao5@gmail.com>
2024-02-09 13:58:46 -0300

    how to create graphs done

--- Python 100 days projects/037 habit track project/main.py
+++ Python 100 days projects/037 habit track project/main.py
@@ -1,6 +1,9 @@
 import requests
 from usefulkeys import token, username
 
+
+
+# 1. Create an user at pixela API
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -11,4 +14,28 @@ user_params = {
 
 }
 
-requests.post(url=pixela_endpoint, params=user_params)
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
+
+
+# 2. Create a graph definition
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+
+
+graph_params = {
+    'id': 'graph1',
+    'name': 'Pages Read',
+    'unit': 'pages',
+    'type': 'int',
+    'color': 'momiji'
+}
+
+
+headers = {
+    "X-USER-TOKEN": token
+}
+
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+print(response.text)
