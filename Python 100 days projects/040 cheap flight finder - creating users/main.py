4af6a85b8b714c941b1ba4ede263a456b42eed21
TonyVSK <arthurdovao5@gmail.com>
2024-02-13 10:16:22 -0300

    creation of users add to sheets, project done

--- Python 100 days projects/040 cheap flight finder - creating users/main.py
+++ Python 100 days projects/040 cheap flight finder - creating users/main.py
@@ -1,4 +1,4 @@
-from usefulkeys import username, projectName, sheetName, authorization
+from usefulkeys import username, projectName, sheetName, authorization, username2, projectName2, sheetName2
 import requests
 
 
@@ -14,6 +14,8 @@ import requests
 # print(response.text)
 # print(response.status_code)
 
+
+## CREATING USER AT THE TABLE TO INSERT THEIR INFORMATIONS 
 # POST USER INFORMATION
 
 print('Welcome to the flight clube')
@@ -27,11 +29,11 @@ while email1 != email2:
     if email1 != email2:
         print("The email don't match; please try again")
 
-sheety_endpoint = f'https://api.sheety.co/{username}/{projectName}/{sheetName}'
+sheety_endpoint = f'https://api.sheety.co/{username2}/{projectName2}/{sheetName2}'
 sheety_params = {
-    'username': username,
-    'projectName': projectName,
-    'sheetName': sheetName
+    'username': username2,
+    'projectName': projectName2,
+    'sheetName': sheetName2
 }
 
 
@@ -55,3 +57,49 @@ users = {
 response = requests.post(url=f'https://api.sheety.co/{username}/{projectName}/{sheetName}', json=users, headers=headers)
 print(response.text)
 print(response.status_code)
+
+# =======================================================================================================================================================
+# =======================================================================================================================================================
+
+
+# POST INFORMATIONS ABOUT FLIGHTS AND EMAIL ALERT
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+import smtplib
+from usefulkeys import my_email, password
+
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+ORIGIN_CITY_IATA = "LON"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    if flight.price < destination["lowestPrice"]:
+        with smtplib.SMTP(my_email, port=587) as connection:
+                    connection.starttls()
+                    connection.login(user=my_email, password=password)
+                    connection.sendmail(
+                        from_addr=my_email, 
+                        to_addrs=email1, 
+                        msg=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+                    )
