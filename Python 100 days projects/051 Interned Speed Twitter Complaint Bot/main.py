702a1d8f274ed4a354cd97f29a53e80953e5f1f7
TonyVSK <arthurdovao5@gmail.com>
2024-02-25 09:03:36 -0300

    project 51 done

--- Python 100 days projects/051 Interned Speed Twitter Complaint Bot/main.py
+++ Python 100 days projects/051 Interned Speed Twitter Complaint Bot/main.py
@@ -1,9 +1,6 @@
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By         
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from time import sleep
 
 URL = 'https://www.speedtest.net'
@@ -38,33 +35,21 @@ except Exception:
     print(f"Error trying to access download: {Exception}")
 
 if download_speed < 50:
+    message = f'Hey @Myinternet, the download speed is about {download_speed}, solve this now!'
     URL2 = 'https://twitter.com/home?lang=pt' # I'm from Brazil :P
 
-    # Keep Chrome browser open after program finishes
+
     chrome_option = webdriver.ChromeOptions()
     chrome_option.add_experimental_option("detach", True)
 
-    driver = webdriver.Chrome(options=chrome_option) # we will work with chrome
-    driver.get(URL)
-
-    # Cookies site button
-    try:
-        sleep(2)
-        cookie = driver.find_element(by=By.XPATH, value='//*[@id="onetrust-accept-btn-handler"]')
-    except:
-        pass
+    driver2 = webdriver.Chrome(options=chrome_option) 
+    driver2.get(URL)
 
 
-    # 'GO' button
-    sleep(2)
-    log_in_button = driver.find_element(by=By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
-    log_in_button.click()
+
+    square = driver2.find_element(by=By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+    square.send_keys(message)
 
 
-    # speed results
-    try:
-        sleep(35) # 35 seconds to load and see my speed
-        download = driver.find_element(by=By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]')
-        download_speed = float(download.text)
-    except Exception:
-        print(f"Error trying to access download: {Exception}")
+    submit_button = driver2.find_element(by=By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div')
+    submit_button.click()
