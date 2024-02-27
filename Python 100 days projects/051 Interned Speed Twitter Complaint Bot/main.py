bf8bd1adb09d17de59ff5df1b9fa680c483060da
TonyVSK <arthurdovao5@gmail.com>
2024-02-27 11:36:59 -0300

    filling inputs form

--- Python 100 days projects/051 Interned Speed Twitter Complaint Bot/main.py
+++ Python 100 days projects/051 Interned Speed Twitter Complaint Bot/main.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
--- Python 100 days projects/053 data entry job automation/main.py
+++ Python 100 days projects/053 data entry job automation/main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # ===================================================================================================================================
-# SCRAPING AMAZON PROCUCT PAGE TO GET PRICE
+# SCRAPING ZULLOW CLONE TO GET ALL DATA TO FILL THE FORM
 URL = "https://appbrewery.github.io/Zillow-Clone/"
 
 
@@ -11,9 +11,9 @@ response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # GETTING HOUSEs ADDRRESS   
-house_links = soup.find_all(name="a", class_='StyledPropertyCardDataArea-anchor')
-house_links = [house.getText().strip() for house in house_links]
-print(house_links)
+house_address = soup.find_all(name="a", class_='StyledPropertyCardDataArea-anchor')
+house_address = [house.getText().strip() for house in house_address]
+print(house_address)
 
 
 # GETTING HOUSE PRICES
@@ -26,3 +26,42 @@ print(house_prices)
 house_links = soup.find_all(name="a", class_='StyledPropertyCardDataArea-anchor')
 house_links = [house.get('href').strip() for house in house_links]
 print(house_links)
+
+
+
+# ===================================================================================================================================
+# FILLING FORM WITH THE DATA USING SELENIUM
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+
+
+URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfGjwLe0zws45o2hwAhCw-BxCiTpgrU6pCLWuK6qej38zfRyg/viewform'
+
+# Keep Chrome browser open after program finishes
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_option) # we will work with chrome
+
+
+
+for i in range(0, len(house_address))
+    driver.get(URL)
+    # ADDRESS INPUT
+    sleep(2)
+    address_input = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    address_input.send_keys(house_address[i], Keys.TAB)
+    # PRICE INPUT
+    price_input = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price_input.send_keys(house_prices[i], Keys.TAB)
+    # LINK INPUT
+    link_input = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_input.send_keys(house_links[i])
+
+    # SEND BUTTON
+    send_button = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+    send_button.click()
+    sleep(2)
