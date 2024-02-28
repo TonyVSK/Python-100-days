13b52624136545657fe49170de1fc7921bdf3c79
TonyVSK <arthurdovao5@gmail.com>
2024-02-28 12:04:54 -0300

    installing flask

--- Python 100 days projects/054 web development with flask/hello_flask/hello.py
+++ Python 100 days projects/054 web development with flask/hello_flask/hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+# terminal command to environment variable: set FLASK_APP=hello.py (it is on documentation)
+@app.route("/") # second command is flask run
 def hello_world():
     return "<p>Hello, World!</p>"
