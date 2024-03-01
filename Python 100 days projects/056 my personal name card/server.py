748e2de87044a65f4634830809a755d58ca3032a
TonyVSK <arthurdovao5@gmail.com>
2024-03-01 11:41:03 -0300

    static folder created

--- Python 100 days projects/056 my personal name card/server.py
+++ Python 100 days projects/056 my personal name card/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 # IF I WANT TO USE MY index.html FILE, I NEED TO CREATE A FOLDER DIRECTORY CALLED TEMPLATES, IT IS FLASK REQUISITE
+# All STATIC FILES, LIKE IMAGES AND CSS FILES, ARE SAVED IN A FOLDER CALLED static. DON'T FORGET TO POINT IT AT index.html
 app = Flask(__name__)
 
 # terminal command to environment variable: set FLASK_APP=server.py (it is on documentation)
