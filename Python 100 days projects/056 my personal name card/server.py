ad1bd109b4eaca21f11ee170f94bcf7f486ec65b
TonyVSK <arthurdovao5@gmail.com>
2024-03-01 11:06:22 -0300

    templates folder created

--- Python 100 days projects/056 my personal name card/server.py
+++ Python 100 days projects/056 my personal name card/server.py
@@ -1,15 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
+# IF I WANT TO USE MY index.html FILE, I NEED TO CREATE A FOLDER DIRECTORY CALLED TEMPLATES, IT IS FLASK REQUISITE
 app = Flask(__name__)
 
 # terminal command to environment variable: set FLASK_APP=server.py (it is on documentation)
 @app.route("/") # second command is flask run
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html") # I need to insert the name of the file who lives in templates folder
 
-@app.route("/bye")
-def say_bye():
-    return "Bye"
 
 # or, you can avoid command run flask of local environment and run the commands just doing this:
 if __name__ == "__main__":
