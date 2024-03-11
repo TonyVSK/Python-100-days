c095fd8162e24496c5ede9391b3a834b3438f31d
TonyVSK <arthurdovao5@gmail.com>
2024-03-11 11:48:20 -0300

    steps 5 and 6 done

--- Python 100 days projects/059 blog flask with css/main.py
+++ Python 100 days projects/059 blog flask with css/main.py
@@ -4,15 +4,56 @@ import random
 from datetime import datetime
 import requests
 
+
+owm_Endpoint = "https://api.npoint.io/73cf3ccba6affe03411f"
+response = requests.get(owm_Endpoint)
+response.raise_for_status()
+blog = response.json()
+
+year = datetime.now().year
+
+
+
 app = Flask(__name__)
 
 # terminal command to environment variable: set FLASK_APP=hello.py (it is on documentation)
 @app.route("/") # second command is flask run
-
 def hello_world():
-    current_year = datetime.now().year
-    random_number = random.randint(1, 100)
-    return render_template("index.html", num=random_number, year=current_year)
+    return render_template("index.html", blog=blog)
+
+
+@app.route("/index.html")
+def index_file():
+    return render_template("index.html", blog=blog)
+
+
+@app.route("/about.html")
+def about_file():
+    return render_template("about.html")
+
+
+@app.route("/contact.html")
+def contact_file():
+    return render_template("contact.html")
+
+
+@app.route("/post.html")
+def post_file():
+    return render_template("post.html")
+
+
+
+    
+@app.route('/post/<identity>')
+def get_post(identity):
+    post = blog[int(identity)-1]
+    return render_template("post.html", post=post, copyright=year)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
 
 
 if __name__ == "__main__":
