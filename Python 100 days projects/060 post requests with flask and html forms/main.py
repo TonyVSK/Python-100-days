e7c0fa9e45619476e7c6f73ce71565f390275601
TonyVSK <arthurdovao5@gmail.com>
2024-03-11 13:13:42 -0300

    new files added

---
+++ Python 100 days projects/060 post requests with flask and html forms/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/73cf3ccba6affe03411f").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
