5976633ec7cd4d5b3f6b2b1dae4a33006d12546f
TonyVSK <arthurdovao5@gmail.com>
2024-03-11 15:20:32 -0300

    first validators done

--- Python 100 days projects/061 advanced forms with flask/main.py
+++ Python 100 days projects/061 advanced forms with flask/main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
-# class MyForm(FlaskForm):
-#     name = StringField('name', validators=[DataRequired()])
+
+# Following the documentation to use Flask Form WTF, the class needs to be in this format:
+    # class MyForm(FlaskForm):
+    #     name = StringField('name', validators=[DataRequired()])
+
+
+
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label="Log in", validators=[DataRequired()])
 
 
 app = Flask(__name__)
@@ -23,9 +29,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
