270d95aee2d40dc6d8245a35370f066837bfe936
TonyVSK <arthurdovao5@gmail.com>
2024-03-12 08:45:51 -0300

    project 63 created

---
+++ Python 100 days projects/063 databases SQL - virtual bookshelf/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method=="POST":
+        information = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(information)
+    print(all_books)
+        
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
